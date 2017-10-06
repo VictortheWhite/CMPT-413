84f951677f10f444693b7071246a6559dd536e12
Chong Guo <armourg@sfu.ca>
2017-10-06 15:10:37 -0700

    change to use dynamic programming instead, acc=88.55

--- segmenter/answer/segment.py
+++ segmenter/answer/segment.py
@@ -11,6 +11,7 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument("-c", "--unigramcounts", dest='counts1w', type=list, nargs='+', default=[os.path.join('data', 'count_1w.txt')], help="unigram counts")
 argparser.add_argument("-b", "--bigramcounts", dest='counts2w', type=list, nargs='+', default=[os.path.join('data', 'count_2w.txt')], help="bigram counts")
 argparser.add_argument("-i", "--inputfile", dest="input", type=str, default=os.path.join('data', 'input'), help="input file to segment")
+argparser.add_argument("-m", "--maxlen", dest ='maxlen', type=int, default=10, help="max possible length for each word")
 argparser.add_argument("-s", "--smooth", dest ='smooth', type=float, default=1.0, help="smoothing parameter")
 argparser.add_argument("-l", "--log", dest='enable_log', type=bool, default=False, help="the flag that enable the log print")
 args = argparser.parse_args()
@@ -27,23 +28,16 @@ class ProbDist(dict):
         self.smoothingfn = smoothingfn or (lambda prob, v, t: (prob + args.smooth) / (v + args.smooth * t))
 
     def __call__(self, key):
-        prob = self.get(key, 0) # if missing, the prob will be 0
-        return self.smoothingfn(float(prob), self.totalvalue, self.totaltype)
+        """Get probability for this key"""
+        prob = self.get(key, 0) # if missing, the prob will be 0 before smoothing
+        if prob == 0 and len(key) > 1:
+            return 1e-200
+        else:
+            return self.smoothingfn(float(prob), self.totalvalue, self.totaltype)
 
     def log_prob(self, key):
         """Return log probability for this key"""
         return math.log(self(key))
-
-class Entry(object):
-    """Entry that stored in heap"""
-    def __init__(self, word, start_position, log_prob, back_pointer):
-        self.word = word
-        self.start_position = start_position
-        self.log_prob = log_prob
-        self.back_pointer = back_pointer
-
-    def __lt__(self, other):
-        return self.start_position < other.start_position
 
 # the default segmenter does not use any probabilities, but you could ...
 prob_dist = ProbDist(args.counts1w)
@@ -60,56 +54,34 @@ with open(args.input) as f:
 
         # the dynamic programming table to store the argmax for every prefix of input
         chart = {}
+        chart[-1] = ("", 1.0) # initial state: empty string with probability 1.0
 
-        # init priority queue (heap)
-        heap = queue.PriorityQueue()
-        missing = True
-        for word in prob_dist:
-            if line.startswith(word):
-                heap.put(Entry(word, 0, prob_dist.log_prob(word), None))
-                missing = False
+        for i in range(len(input_words)):
+            for j in range(1, args.maxlen + 1):
+                if i - j + 1 < 0:
+                    continue
+                word = "".join(input_words[i-j+1:i+1])
+                prob = prob_dist.log_prob(word)
+                _, prev_prob = chart[i - j]
                 if args.enable_log:
-                    print("==> put entry: ", (word, 0, prob_dist.log_prob(word), None))
-        if missing:
-            new_entry = Entry(line[0], 0, prob_dist.log_prob(line[0]), None)
-            heap.put(new_entry)
-            if args.enable_log:
-                print("==> put entry: ", (line[0], 0, prob_dist.log_prob(line[0]), None))
-
-        # iteratively fill in chart[i] for all i
-        while not heap.empty():
-            entry = heap.get()
-            if args.enable_log:
-                print("==> get entry: ", (entry.word, entry.start_position, entry.log_prob, entry.back_pointer))
-            end_position = entry.start_position + len(entry.word) - 1
-            if end_position in chart and entry.log_prob <= chart[end_position].log_prob:
-                continue
-            chart[end_position] = entry
-
-            next_start_position = end_position + 1
-            if next_start_position != len(input_words):
-                missing = True
-                for word in prob_dist:
-                    if line[next_start_position:].startswith(word):
-                        new_entry = Entry(word, next_start_position, prob_dist.log_prob(word), entry)
-                        heap.put(new_entry)
-                        missing = False
-                        if args.enable_log:
-                            print("==> put entry: ", (word, next_start_position, prob_dist.log_prob(word), entry))
-                if missing:
-                    new_entry = Entry(line[next_start_position], next_start_position, prob_dist.log_prob(line[next_start_position]), entry)
-                    heap.put(new_entry)
+                    print("==> Check: ", i, j, word, prob, prev_prob)
+                if i not in chart:
+                    chart[i] = (word, prev_prob + prob)
                     if args.enable_log:
-                        print("==> put entry: ", (line[next_start_position], next_start_position, prob_dist.log_prob(line[next_start_position]), entry))
-
-        if args.enable_log:
-            print("==> Chart: ", chart)
+                        print("==> New: ", i, word, prev_prob + prob)
+                else:
+                    _, best_prob = chart[i]
+                    if prev_prob + prob > best_prob:
+                        chart[i] = (word, prev_prob + prob)
+                        if args.enable_log:
+                            print("==> Update: ", i, word, prev_prob + prob)
 
         # get the best segmentation
+        index = len(input_words) - 1
         segmentation = []
-        segmented_entry = chart[len(input_words) - 1]
-        while segmented_entry is not None:
-            segmentation.append(segmented_entry.word)
-            segmented_entry = segmented_entry.back_pointer
+        while index >= 0:
+            segmented_word, _ = chart[index]
+            segmentation.append(segmented_word)
+            index -= len(segmented_word)
         segmentation.reverse()
         print(" ".join(segmentation))
