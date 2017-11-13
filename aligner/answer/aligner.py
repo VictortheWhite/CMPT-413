e90114d5715445d0518af9dfcc20be1445cd138d
Chong Guo <armourg@sfu.ca>
2017-11-12 20:32:46 -0800

    fixed IBM2 bug

--- aligner/answer/aligner.py
+++ aligner/answer/aligner.py
@@ -12,100 +12,102 @@ argparser.add_argument("-d", "--datadir", dest="datadir", default="data", help="
 argparser.add_argument("-p", "--prefix", dest="fileprefix", default="hansards", help="prefix of parallel data files (default=hansards)")
 argparser.add_argument("-e", "--english", dest="english", default="en", help="suffix of English (target language) filename (default=en)")
 argparser.add_argument("-f", "--french", dest="french", default="fr", help="suffix of French (source language) filename (default=fr)")
-argparser.add_argument("-s", "--smoothing", dest="smooth", default=0.001, type=float, help="add_n smoothing value")
-argparser.add_argument("-S", "--Smoothing", dest="Smooth", default=0.003, type=float, help="add_n smoothing value for distortion")
+argparser.add_argument("-t", "--smoothing-t", dest="smooth_t", default=0.001, type=float, help="add_n smoothing value for t")
+argparser.add_argument("-q", "--smoothing-q", dest="smooth_q", default=0.0004, type=float, help="add_n smoothing value for q(distortion)")
 argparser.add_argument("-n", "--num_sentences", dest="num_sents", default=2**64, type=int, help="Number of sentences to use for training and alignment")
 argparser.add_argument("-i", "--num_iteration", dest="num_iter", default=5, type=int, help="Number of iteration/epoch number")
 args = argparser.parse_args()
 
 def align(bitext):
-    t = defaultdict(float)
-    a = defaultdict(float)
-    
     sys.stderr.write("Init parameters...\n")
+    t = defaultdict(float)
+    q = defaultdict(float)
+
     count_e = defaultdict(float)
     count_f = defaultdict(float)
-    count_ef = defaultdict(float)
+    count_fe = defaultdict(float)
+
     for f, e in bitext:
-        I = len(f)
-        J = len(e)
+        l, m = len(e), len(f)
+        for f_i in set(f):
+            count_f[f_i] += 1
+        for e_j in set(e).union({None}):
+            count_e[e_j] += 1
         for i, f_i in enumerate(f):
             for j, e_j in enumerate(e):
-                count_f[f_i] += 1
-                count_e[e_j] += 1
-                count_ef[(f_i, e_j)] += 1
-                a[j, i, I, J] = 1.0 / (J+1)
-            a[-1, i, I, J] = 1.0 / (J+1)
-        t[f_i, None] = 1.0 / (I * J)
-    for f, e in count_ef:
-        t[(f, e)] = count_ef[(f, e)] / (count_e[e] * count_f[f])
+                count_fe[(f_i, e_j)] += 1
+                q[(j, i, l, m)] = 1.0 / (l + 1)
+            count_fe[(f_i, None)] += 1
+            q[(None, i, l, m)] = 1.0 / (l + 1)
 
-    V, S = 100000, 10000
+    for f, e in count_fe:
+        t[(f, e)] = count_fe[(f, e)] / (count_e[e] * count_f[f])
+
+    sys.stderr.write("%d %d\n" % (len(count_f), len(count_e)))
+    V_t, V_q = 100000, 10000
+
     # traning
-    for T in range(args.num_iter):
-        sys.stderr.write("Starting Iteration %d ...\n" % T)
+    for k in range(args.num_iter):
+        sys.stderr.write("Starting Iteration %d ...\n" % k)
         expected_count_t_fe = defaultdict(float)
         expected_count_t_e = defaultdict(float)
-        expected_count_a_fe = defaultdict(float)
-        expected_count_a_e = defaultdict(float)
+        expected_count_q_fe = defaultdict(float)
+        expected_count_q_e = defaultdict(float)
 
-        for k, (f, e) in enumerate(bitext):
-            if k % 10000 == 0:
-                 sys.stderr.write("     %d\n" % k)
-            I, J = len(f), len(e)
-            for i in range(I):
+        for f, e in bitext:
+            l, m = len(e), len(f)
+            for i, f_i in enumerate(f):
                 # calculate the normalization term
-                z = t[(f[i], None)] * a[(-1, i, I, J)]
-                for j in range(J):
-                    z += t[(f[i], e[j])] * a[(j, i, I, J)]
+                z = t[(f_i, None)] * q[(None, i, l, m)]
+                for j, e_j in enumerate(e):
+                    z += t[(f_i, e_j)] * q[(j, i, l, m)]
                 # calculate expected count
-                for j in range(J):
-                    c = t[(f[i], e[j])] * a[(j, i, I, J)] / z
-                    expected_count_t_fe[(f[i], e[j])] += c
-                    expected_count_t_e[e[j]] += c
-                    expected_count_a_fe[(j, i, I, J)] += c
-                    expected_count_a_e[(i, I, J)] += c
+                for j, e_j in enumerate(e):
+                    c = t[(f_i, e_j)] * q[(j, i, l, m)] / z
+                    expected_count_t_fe[(f_i, e_j)] += c
+                    expected_count_t_e[e_j] += c
+                    expected_count_q_fe[(j, i, l, m)] += c
+                    expected_count_q_e[(i, l, m)] += c
 
-                c = t[(f[i], None)] * a[(-1, i, I, J)] / z
+                c = t[(f_i, None)] * q[(None, i, l, m)] / z
                 expected_count_t_fe[(f_i, None)] += c
                 expected_count_t_e[None] += c
-                expected_count_a_fe[(-1, i, I, J)] += c
-                expected_count_a_e[(i, I, J)] += c
+                expected_count_q_fe[(None, i, l, m)] += c
+                expected_count_q_e[(i, l, m)] += c
         
         # smoothing
         for f, e in expected_count_t_fe:
-            t[(f, e)] = (expected_count_t_fe[(f, e)] + args.smooth) / (expected_count_t_e[e] + args.smooth * V)
-        for i, j, I, J in expected_count_a_fe:
-            a[(i, j, I, J)] = (expected_count_a_fe[(i, j, I, J)] + args.Smooth) / (expected_count_a_e[j, I, J] + args.Smooth * S)
+            t[(f, e)] = (expected_count_t_fe[(f, e)] + args.smooth_t) / (expected_count_t_e[e] + args.smooth_t * V_t)
+        for j, i, l, m in expected_count_q_fe:
+            q[(j, i, l, m)] = (expected_count_q_fe[(j, i, l, m)] + args.smooth_q) / (expected_count_q_e[i, l, m] + args.smooth_q * V_q)
     
-    alignments = []
     # align and output result
+    sys.stderr.write("Save alignment...\n")
+    alignments = []
     for f, e in bitext:
-        I, J = len(f), len(e)
+        l, m = len(e), len(f)
         alignments.append([])
         for i, f_i in enumerate(f):
-            best_p = t[(f_i, None)] * a[-1, i, I, J]
+            best_p = t[(f_i, None)] * q[None, i, l, m]
             best_j = -1
             for j, e_j in enumerate(e):
-                p = t[(f_i, e_j)] * a[-1, i, I, J]
+                p = t[(f_i, e_j)] * q[j, i, l, m]
                 if p > best_p:
                     best_p = p
                     best_j = j
             if best_j >= 0:
                 alignments[-1].append((i, best_j))
-                #sys.stdout.write("%i-%i " % (i, best_j))
-        #sys.stdout.write("\n")
-    return alignments
 
+    return alignments
 
 if __name__ == '__main__':
     sys.stderr.write("Read data...\n")
     f_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.french)
     e_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.english)
-    bitext = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(f_data), open(e_data)))[:args.num_sents]]
+    bitext_1 = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(f_data), open(e_data)))[:args.num_sents]]
     bitext_2 = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(e_data), open(f_data)))[:args.num_sents]]
 
-    aligns_1 = align(bitext)
+    aligns_1 = align(bitext_1)
     aligns_2 = align(bitext_2)
 
     # output 2 intersection of alignments
@@ -114,23 +116,3 @@ if __name__ == '__main__':
             if (j, i) in aligns_2[k]:
                 sys.stdout.write("%i-%i " % (i, j))
         sys.stdout.write("\n")
-        
-
-    # # calculate V (length of f words)
-    # f_count = defaultdict(int)
-    # for f, e in bitext:
-    #     for f_i in set(f):
-    #         f_count[f_i] += 1
-    # V = len(f_count)
-
-    # # calculate the length of all distortions
-    # a_count = set()
-    # for f, e in bitext:
-    #     I, J = len(f), len(e)
-    #     for i in range(I):
-    #         for j in range(-1, J):
-    #             a_count.add((j, i, I, J))
-    # S = len(a_count)
-
-    # init t and a
-    
