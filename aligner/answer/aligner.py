b8efa01b0a0f7cb481d61d3c75c0b016efd3699b
Victor <vhe@sfu.ca>
2017-11-12 17:39:24 -0800

    train both ways and intersect result

--- aligner/answer/aligner.py
+++ aligner/answer/aligner.py
@@ -7,45 +7,22 @@ import argparse
 
 from collections import defaultdict
 
-if __name__ == '__main__':
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("-d", "--datadir", dest="datadir", default="data", help="data directory (default=data)")
-    argparser.add_argument("-p", "--prefix", dest="fileprefix", default="hansards", help="prefix of parallel data files (default=hansards)")
-    argparser.add_argument("-e", "--english", dest="english", default="en", help="suffix of English (target language) filename (default=en)")
-    argparser.add_argument("-f", "--french", dest="french", default="fr", help="suffix of French (source language) filename (default=fr)")
-    argparser.add_argument("-s", "--smoothing", dest="smooth", default=0.001, type=float, help="add_n smoothing value")
-    argparser.add_argument("-S", "--Smoothing", dest="Smooth", default=0.005, type=float, help="add_n smoothing value for distortion")
-    argparser.add_argument("-n", "--num_sentences", dest="num_sents", default=2**64, type=int, help="Number of sentences to use for training and alignment")
-    argparser.add_argument("-i", "--num_iteration", dest="num_iter", default=5, type=int, help="Number of iteration/epoch number")
-    args = argparser.parse_args()
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-d", "--datadir", dest="datadir", default="data", help="data directory (default=data)")
+argparser.add_argument("-p", "--prefix", dest="fileprefix", default="hansards", help="prefix of parallel data files (default=hansards)")
+argparser.add_argument("-e", "--english", dest="english", default="en", help="suffix of English (target language) filename (default=en)")
+argparser.add_argument("-f", "--french", dest="french", default="fr", help="suffix of French (source language) filename (default=fr)")
+argparser.add_argument("-s", "--smoothing", dest="smooth", default=0.001, type=float, help="add_n smoothing value")
+argparser.add_argument("-S", "--Smoothing", dest="Smooth", default=0.003, type=float, help="add_n smoothing value for distortion")
+argparser.add_argument("-n", "--num_sentences", dest="num_sents", default=2**64, type=int, help="Number of sentences to use for training and alignment")
+argparser.add_argument("-i", "--num_iteration", dest="num_iter", default=5, type=int, help="Number of iteration/epoch number")
+args = argparser.parse_args()
 
-    sys.stderr.write("Read data...\n")
-    f_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.french)
-    e_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.english)
-    bitext = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(f_data), open(e_data)))[:args.num_sents]]
-
-    sys.stderr.write("Init parameters...\n")
-
-    # # calculate V (length of f words)
-    # f_count = defaultdict(int)
-    # for f, e in bitext:
-    #     for f_i in set(f):
-    #         f_count[f_i] += 1
-    # V = len(f_count)
-
-    # # calculate the length of all distortions
-    # a_count = set()
-    # for f, e in bitext:
-    #     I, J = len(f), len(e)
-    #     for i in range(I):
-    #         for j in range(-1, J):
-    #             a_count.add((j, i, I, J))
-    # S = len(a_count)
-
-    # init t and a
+def align(bitext):
     t = defaultdict(float)
     a = defaultdict(float)
     
+    sys.stderr.write("Init parameters...\n")
     count_e = defaultdict(float)
     count_f = defaultdict(float)
     count_ef = defaultdict(float)
@@ -73,8 +50,8 @@ if __name__ == '__main__':
         expected_count_a_e = defaultdict(float)
 
         for k, (f, e) in enumerate(bitext):
-            if k % 100 == 0:
-                print('    ', k)
+            if k % 10000 == 0:
+                 sys.stderr.write("     %d\n" % k)
             I, J = len(f), len(e)
             for i in range(I):
                 # calculate the normalization term
@@ -100,10 +77,12 @@ if __name__ == '__main__':
             t[(f, e)] = (expected_count_t_fe[(f, e)] + args.smooth) / (expected_count_t_e[e] + args.smooth * V)
         for i, j, I, J in expected_count_a_fe:
             a[(i, j, I, J)] = (expected_count_a_fe[(i, j, I, J)] + args.Smooth) / (expected_count_a_e[j, I, J] + args.Smooth * S)
-
+    
+    alignments = []
     # align and output result
     for f, e in bitext:
         I, J = len(f), len(e)
+        alignments.append([])
         for i, f_i in enumerate(f):
             best_p = t[(f_i, None)] * a[-1, i, I, J]
             best_j = -1
@@ -113,5 +92,45 @@ if __name__ == '__main__':
                     best_p = p
                     best_j = j
             if best_j >= 0:
-                sys.stdout.write("%i-%i " % (i, best_j))
+                alignments[-1].append((i, best_j))
+                #sys.stdout.write("%i-%i " % (i, best_j))
+        #sys.stdout.write("\n")
+    return alignments
+
+
+if __name__ == '__main__':
+    sys.stderr.write("Read data...\n")
+    f_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.french)
+    e_data = "%s.%s" % (os.path.join(args.datadir, args.fileprefix), args.english)
+    bitext = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(f_data), open(e_data)))[:args.num_sents]]
+    bitext_2 = [[sentence.strip().split() for sentence in pair] for pair in list(zip(open(e_data), open(f_data)))[:args.num_sents]]
+
+    aligns_1 = align(bitext)
+    aligns_2 = align(bitext_2)
+
+    # output 2 intersection of alignments
+    for k, a in enumerate(aligns_1):
+        for i, j in a:
+            if (j, i) in aligns_2[k]:
+                sys.stdout.write("%i-%i " % (i, j))
         sys.stdout.write("\n")
+        
+
+    # # calculate V (length of f words)
+    # f_count = defaultdict(int)
+    # for f, e in bitext:
+    #     for f_i in set(f):
+    #         f_count[f_i] += 1
+    # V = len(f_count)
+
+    # # calculate the length of all distortions
+    # a_count = set()
+    # for f, e in bitext:
+    #     I, J = len(f), len(e)
+    #     for i in range(I):
+    #         for j in range(-1, J):
+    #             a_count.add((j, i, I, J))
+    # S = len(a_count)
+
+    # init t and a
+    
